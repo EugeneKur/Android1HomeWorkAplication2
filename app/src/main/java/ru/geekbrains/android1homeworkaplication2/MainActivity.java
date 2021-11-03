020680c2c53abb222f5bb4fed56d9e181784365c
EugeneKur <ew.kurilov@gmail.com>
2021-11-03 16:45:26 +0300

    Home Work5

--- app/src/main/java/ru/geekbrains/android1homeworkaplication2/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1homeworkaplication2/MainActivity.java
@@ -3,6 +3,7 @@ package ru.geekbrains.android1homeworkaplication2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -73,6 +74,11 @@ public class MainActivity extends AppCompatActivity {
             parametrs.setFIRST_NUMBER("");
             parametrs.setSECOND_NUMBER("");
             parametrs.setOPERATION('x');
+        });
+
+        findViewById(R.id.button_setting).setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivitySettings.class);
+            startActivity(intent);
         });
 
 
